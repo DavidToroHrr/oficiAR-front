70f7cb58f62913f205ba403400ade1d9a1d34892
David Toro <davidesteban0736@.com>
2026-03-06 10:00:13 -0500

    colors added. files structure defined

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ButtonComponent from './components/Button/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <p>HOLA. ESTE ES EL INICIO DE OFICIAR</p>
+      <p >HOLA. ESTE ES EL INICIO DE OFICIAR</p>
+
+      <ButtonComponent />
     </div>
   )
 }
 
 export default App
+// siempre qiue hay un retorno de un componente de react, debe estar envuelto en un div...
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+
+export default function Button(){
+    
+    return(
+        <div className="button-component">
+            <button>Button</button>
+        </div>
+    )
+
+}
+
+

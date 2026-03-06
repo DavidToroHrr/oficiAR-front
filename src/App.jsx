d3dcca38ac622bdc1e1e3e1620c4899adddd9090
David Toro <davidesteban0736@.com>
2026-03-06 10:16:06 -0500

    navigation defined

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,28 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ButtonComponent from './components/Button/Button'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login/Login';
+import WorkerRegister from './pages/WorkerRegister/WorkerRegister';
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <p >HOLA. ESTE ES EL INICIO DE OFICIAR</p>
+    <BrowserRouter>
+      <Routes>
+        {/* Ruta para el inicio (cuando entras a localhost:5173/) */}
+        <Route path="/" element={<h1>Bienvenido al Inicio de OficiAR</h1>} />
+        
+        {/* Ruta para las solicitudes (localhost:5173/solicitudes) */}
+        <Route path="/solicitudes" element={<h1>Panel de Solicitudes</h1>} />
+        
+        {/* Puedes ir agregando todas las que necesites para tu entrega */}
+        <Route path="/login" element={<Login />} />
 
-      <ButtonComponent />
-    </div>
+        <Route path="/worker-register" element={<WorkerRegister />} />
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/WorkerRegister/WorkerRegister.tsx
@@ -0,0 +1,9 @@
+export default function WorkerRegister(){
+    return(
+        <div>
+            <h1>WorkerRegister... Jacobo y Vick son gays</h1>
+        </div>
+    )
+}
+
+

84c261d209f24f40a8142a1a406dd07decb999e5
victormanuel20 <victormanuel20034@gmail.com>
2026-03-06 11:06:20 -0500

    Add image in login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,11 @@
 import Button from "../../components/Button/Button";
+import assets from "../../assets/loginImage.jpeg";
 
 export default function Login(){
     return(
         <div>
             <h1>Login... Jacobo y Vick son gays</h1>
+            <img src={assets} alt="Login" />
             <Button>
 
             </Button>

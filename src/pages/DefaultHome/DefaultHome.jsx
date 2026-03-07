d21d5decc6ac4acdbe591cb075a277bfde796e78
David Toro <davidesteban0736@.com>
2026-03-07 00:39:01 -0500

    feat: add header, hero slogan and search bar

---
+++ src/pages/DefaultHome/DefaultHome.jsx
@@ -0,0 +1,32 @@
+import Header from "../../components/Header/Header"
+import Button from "../../components/Button/Button"
+import SearchBar from "../../components/SearchBar/SearchBar"
+export default function DefaultHome(){
+    
+    return(
+        <div>
+            <Header> 
+                <a href="">Iniciar Sesion</a>
+                <a href="">Registro</a>
+
+                <Button>
+                    Hazte Officer
+                </Button>
+            </Header>
+
+
+        <p className="pt-35 text-5xl font-black text-very-dark-blue text-center ">
+            Soluciones <br/>
+            <span className="text-light-blue">profesionales<br/></span> 
+            para tu hogar<br/> 
+            <span className="text-light-blue">al instante</span>
+        </p>
+
+        <SearchBar>
+        </SearchBar>
+
+
+            
+        </div>
+    )
+}
--- src/pages/WorkerRegister/WorkerRegister.jsx
+++ src/pages/WorkerRegister/WorkerRegister.jsx
@@ -1,8 +1,14 @@
 export default function WorkerRegister(){
     return(
         <div>
+            <h1 className="text-3xl text-light-blue bg-very-dark-blue p-4">
+                ¡Tailwind v4 está funcionando!
+            </h1>
             <h1>WorkerRegister... Jacobo y Vick son gays</h1>
+            
         </div>
+
+        
     )
 }
 

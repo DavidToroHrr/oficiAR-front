87d552bfdb54cf89c378280cefbc45057517d200
JacoboAP1 <jacobo.arroyavep@autonoma.edu.co>
2026-04-26 15:44:48 -0500

    Refactoring sizes and responsive on DefaultHome view

--- src/components/RatingComponent/RatingComponent.jsx
+++ src/components/RatingComponent/RatingComponent.jsx
@@ -5,7 +5,7 @@ export default function RatingComponent({children,descriptionCalification}){
             {children}
             {/* en children van las estrellas que son RatingStar */}
 
-            <p className="ml-2 text-sm font-light  dark:text-gray-400">{descriptionCalification}</p>
+            <p className="ml-2 text-sm font-light dark:text-gray-400">{descriptionCalification}</p>
         </Rating>
         
     )
--- src/components/TestimonialCard/TestimonialCard.jsx
+++ src/components/TestimonialCard/TestimonialCard.jsx
@@ -4,14 +4,14 @@ import { RatingStar } from "flowbite-react";
 export default function TestimonialCard({name, testimonial, calification,descriptionCalification,date}){
     calification = parseInt(calification)
     return(
-        <article className=" bg-very-light-gray w-[313px] min-h-[236px] p-4 rounded-lg shadow-md flex flex-col justify-between">
+        <article className=" bg-very-light-gray w-full max-w-[313px] p-5 rounded-lg shadow-md flex flex-col gap-4">
             <div className="flex items-center justify-between">
-                <p className="font-bold text-4xl">{name}</p>
+                <p className="font-bold text-2xl">{name}</p>
                 <RatingComponent descriptionCalification={descriptionCalification}>
-                {Array.from({ length: calification }).map((_, index) => (
-                    <RatingStar key={index} />
-                ))}
-            </RatingComponent>
+                    {Array.from({ length: calification }).map((_, index) => (
+                        <RatingStar key={index} />
+                    ))}
+                </RatingComponent>
 
             </div>
             <p className="">{testimonial}</p>

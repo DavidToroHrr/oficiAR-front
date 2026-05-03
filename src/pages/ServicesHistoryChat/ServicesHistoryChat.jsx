4c515a8afee7d83ca6ec978d76fc4b27cca1484a
victormanuel20 <victormanuel20034@gmail.com>
2026-05-03 15:08:59 -0500

    Navigation on services-history-chat

--- src/pages/ServicesHistoryChat/ServicesHistoryChat.jsx
+++ src/pages/ServicesHistoryChat/ServicesHistoryChat.jsx
@@ -2,8 +2,12 @@ import InteractiveChat from "../../components/InteractiveChat/InteractiveChat";
 import Logo from "../../assets/logo.png";
 import Button from "../../components/Button/Button";
 import Background from "../../assets/chat-background.png";
+import useFormNavigation from "../../hooks/useFormNavigation";
 
 export default function ServicesHistoryChat() {
+
+    const { goBack } = useFormNavigation();
+
     const chatConCliente = [
         { id: 1, text: "Hola worker, ¿en qué te puedo ayudar?", sender: "them", time: "14:30" },
         { id: 2, text: "Hola, ¿me puedes indicar tu dirección de residencia? Por favor", sender: "me", time: "14:32" },
@@ -18,7 +22,11 @@ export default function ServicesHistoryChat() {
         >
 
             <div className="flex flex-col items-center gap-15">
-                <img src={Logo} className="w-[247px] h-[55px] mt-5" />
+                <img 
+                src={Logo} 
+                className="w-[247px] h-[55px] mt-5 cursor-pointer" 
+                onClick={() => goBack()}
+                />
 
                 <h2 className="mb-4 text-4xl md:text-[25px] font-extrabold text-center pt-5">
                     En unos breves instates el cliente <br/> se pondrá en contacto contigo
@@ -29,10 +37,13 @@ export default function ServicesHistoryChat() {
             </div>
 
             {/* self-end se pone a flotar a la derecha ignorando el flex del contenedor padre */}
-            <Button className="bg-light-blue text-white px-8 py-2.5 rounded-[8px] font-bold shadow-md mt-8
-                                self-end mr-20 mb-10 w-[180px] h-[71px] text-[20px] flex items-center justify-center">
+            <Button 
+                className="bg-light-blue text-white px-8 py-2.5 rounded-[8px] font-bold shadow-md mt-8 self-end mr-20 mb-10 w-[180px] h-[71px] text-[20px] flex items-center justify-center"
+                onClick={() => goBack()}
+            >
                 Volver al <br/> historial
             </Button>
+            
         </div>
     )
 }
